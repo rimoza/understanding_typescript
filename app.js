03f906e2d5e30b3e6073332fe2b8f920c5a9b3a2
rimoza <ridwan.maxamed1@gmail.com>
2022-10-18 23:30:38 -0700

    feat: using types

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Your code is going here...');
+//Using types 
+//number => All types of number are same float, decimal and so on
+//string
+//boolean => there is no truthy and falsy types.
+function add(num1, num2) {
+    return num1 + num2;
+}
+// const number1 = '5'; Argument of type 'string' is not assignable to parameter of type 'number'
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
