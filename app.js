a40760ce8931b6ba7fc07445e3df0d52fbef4941
rimoza <ridwan.maxamed1@gmail.com>
2022-10-22 23:39:05 -0700

    feat: working with enums

--- app.js
+++ app.js
@@ -26,3 +26,72 @@ for (var _i = 0, _a = person1.hobbies; _i < _a.length; _i++) {
     // console.log(hobby.map()) =>Property 'map' does not exist on type 'string'.
 }
 console.log(person1.name);
+// Working with Tuples
+var person2 = {
+    name: 'Ridwan Mohammed',
+    age: 25,
+    hobbies: ['cooking', 'playing'],
+    role: [1, 'author']
+};
+person2.role.push(1, 'admin');
+// person2.role = ['2'] Type 'string' is not assignable to type 'number'.
+/* Tuples [1,2] [1, 'hello']
+A tuple is a TypeScript type that works like an array with some special considerations:
+ The number of elements of the array is fixed.
+ The type of the elements is known.
+ The type of the elements of the array need not be the same.
+*/
+// Working with Enums
+/*
+1. Enums are one of the few features TypeScript has which is not a type-level extension of JavaScript.
+2. Enums allow a developer to define a set of named constants.
+3. Using enums can make it easier to document intent(ujeedada).
+4. Can make it easier to create a set of distinct cases(cases kala duwan).
+5. TypeScript provides both numeric and string-based enums.
+6.An enum can be defined using the 'enum' keyword.
+*/
+//Numeric enums
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+    Direction[Direction["Left"] = 3] = "Left";
+    Direction[Direction["Right"] = 4] = "Right";
+})(Direction || (Direction = {}));
+/**
+ * Above, we have a numeric enum where Up is initialized with 1. All of the following members are auto-incremented from that point on. In other words, Direction.Up has the value 1, Down has 2, Left has 3, and Right has 4.
+
+If we wanted, we could leave off the initializers entirely:
+ */
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+;
+// String enums
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "UP";
+    Direction2["Down"] = "DOWN";
+    Direction2["Left"] = "LEFT";
+    Direction2["Right"] = "RIGHT";
+})(Direction2 || (Direction2 = {}));
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["USER_READ"] = 1] = "USER_READ";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person3 = {
+    name: 'Ridwan Mohammed',
+    age: 25,
+    hobbies: ['cooking', 'playing'],
+    role: Role.ADMIN
+};
+if (person3.role === Role.AUTHOR) {
+    console.log('Is admin');
+}
+;
--- app.ts
+++ app.ts
@@ -55,4 +55,56 @@ A tuple is a TypeScript type that works like an array with some special consider
  The number of elements of the array is fixed.
  The type of the elements is known. 
  The type of the elements of the array need not be the same.
+*/ 
+
+// Working with Enums
+
+/*
+1. Enums are one of the few features TypeScript has which is not a type-level extension of JavaScript.
+2. Enums allow a developer to define a set of named constants. 
+3. Using enums can make it easier to document intent(ujeedada).
+4. Can make it easier to create a set of distinct cases(cases kala duwan).
+5. TypeScript provides both numeric and string-based enums.
+6.An enum can be defined using the 'enum' keyword.
 */
+
+//Numeric enums
+
+enum Direction  {
+    Up = 1,
+    Down,
+    Left,
+    Right
+}
+
+/**
+ * Above, we have a numeric enum where Up is initialized with 1. All of the following members are auto-incremented from that point on. In other words, Direction.Up has the value 1, Down has 2, Left has 3, and Right has 4.
+
+If we wanted, we could leave off the initializers entirely:
+ */
+
+enum Direction1 {
+    Up,
+    Down,
+    Left,
+    Right
+};
+
+// String enums
+enum Direction2 {
+    Up = "UP",
+    Down = "DOWN",
+    Left = "LEFT",
+    Right = "RIGHT",
+  }
+enum Role {ADMIN, USER_READ, AUTHOR}
+const person3= {
+    name:'Ridwan Mohammed',
+    age: 25,
+    hobbies: ['cooking', 'playing'], // hobbies: string[]
+    role: Role.ADMIN
+};
+
+if(person3.role === Role.AUTHOR){
+    console.log('Is admin')
+};
