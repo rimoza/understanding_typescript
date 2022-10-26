4e6b20da2c93e213a4c6cf70cd73a58e762155ae
rimoza <ridwan.maxamed1@gmail.com>
2022-10-26 00:45:55 -0700

    feat: Type Aliases & Object Types

--- app.ts
+++ app.ts
@@ -18,4 +18,41 @@ const combineAges = combine(30, 26);
 console.log(combineAges);
 
 const combineNames = combine('Ridwan', 'Mohamed');
-console.log(combineNames)
+console.log(combineNames);
+
+// Type Aliases & Object Types
+
+// Type alianses can be used to "create" your own types. You're not limited to storing union types through 
+// -You can also provide an alias to a (possible complex) object type.
+// For example 
+type UserEx = {
+    name: string,
+    age: number
+};
+const u1: UserEx = {
+    name:  'Ridwan',
+    age: 24
+}; 
+
+//This allows you to avoid unnecessary repetition and manage types centrally.
+
+
+// For example, you can simplify this code:
+
+function greet(user: {name: string, age: number}){
+ console.log('Hi, I am' + user.name);
+};
+
+function isOlder(user: {name: string, age: number}, checkAge: number){
+    return checkAge > user.age;
+}
+//To
+
+type User = {name: string, age: number};
+
+function greetSimple(user: User){
+    console.log('Hi I am simple' + user.name);
+};
+function greetOlder(user: User, checkAge: number) {
+    return checkAge > user.age;
+  }; 
