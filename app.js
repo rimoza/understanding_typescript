3739bc1efe6f4361d19c51c1a29b0ac3266a3e9d
rimoza <ridwan.maxamed1@gmail.com>
2022-10-26 07:03:13 -0700

    feat: functions as types

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ function printResult(result) {
     console.log('Result:' + result);
 }
 printResult(add(4, 6));
+// FUnctions as types
+//  let combineFunctions: Function; but the problem is that only recognizes that's a function
+var combineFunctions;
+combineFunctions = add;
+//  combineFunctions = 5; Type script doesn't recognize and produces undefined //Function Return Types & "void"
+//  combineFunctions = printResult;  We get unexpected result 
+/* combineFunctions = printResult; Type '(result: number) => void' is not assignable to type '(a: number, b: number) => number'.
+  Type 'void' is not assignable to type 'number'.
+  */
+console.log(combineFunctions(8, 8));
